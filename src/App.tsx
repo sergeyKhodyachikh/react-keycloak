8faeb8ecc1981254ec5960adf3ccaa45d4f251f5
sergeyKhodyachikh <sergey.khodyachikh@devalore.com>
2023-01-08 15:34:26 +0200

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,50 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-
-
-const handleCode = async (code: string | null) => {
-  if(code) {
-    console.log('happen');
-    const { data } = await axios.post('/api/auth/token', { code })
-    console.log(data);
-    const { accessToken, refreshToken } = data
-    console.log(accessToken);
-    localStorage.setItem('accessToken', accessToken);
-  }
-}
-
-
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [tokens, setTokens] = useState({ accessToken: '', refreshToken: '' });
+
+  console.log(tokens.accessToken);
+  
+
+  const handleCode = async (code: string | null) => {
+    if (code) {
+      const { data } = await axios.post("/api/auth/token", { code });
+      const { accessToken, refreshToken } = data;
+      setTokens({ accessToken, refreshToken })
+    }
+  };
+
   useEffect(() => {
-    const params = (new URL(document.location.toString())).searchParams;
-    const code = params.get('code')
-    handleCode(code)
+    const params = new URL(document.location.toString()).searchParams;
+    const code = params.get("code");
+    // handleCode(code);
     // window.history.pushState({}, document.title, window.location.pathname)
-  }, [])
+  }, []);
+
+  
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        accessToken: <br /> <code style={{ whiteSpace: 'nowrap'}}>{tokens.accessToken}</code>
+        <br />
+        refreshToken: <br /> 
+        <code>{tokens.refreshToken}</code>
         </p>
         <a
           className="App-link"
-          href="http://routines.com/auth/realms/client/protocol/openid-connect/auth?response_type=code&client_id=web&redirect_uri=http://routines.com"
-          >
+          href="http://routines.com/auth/realms/users/protocol/openid-connect/auth?response_type=code&client_id=web&redirect_uri=https://routines.com"
+        >
           Login
         </a>
+
       </header>
+
     </div>
   );
 }
